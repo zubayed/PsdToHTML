9b5c9ccba6c19cd3c30a7e966409a240a8621514
zubayed <zubayed141023@gmail.com>
2020-09-27 23:01:59 +0600

    Add files via upload

---
+++ psdToHtmlProject/resources/js/main.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+    $(".js--services-section").waypoint(function(direction){
+        if (direction=="down") {
+            $("nav").addClass("sticky");
+        } else {
+            $("nav").removeClass("sticky");
+        }
+    });
+    var mixer = mixitup('.container');
+});
+function openNav(){
+    document.getElementById("myNav").style.width="100%";
+}
+function closeNav(){
+    document.getElementById("myNav").style.width="0";
+}
